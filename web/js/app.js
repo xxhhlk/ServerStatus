6fc2b5dd806689750ca400d95bc5530f866c69ff
xxhhlk <xxhhlk0@gmail.com>
2026-08-27 03:51:10 +0800

    update tooltip min

--- web/js/app.js
+++ web/js/app.js
@@ -369,7 +369,7 @@ function alertStats(){
 }
 
 function normalizeServersToolbarState(){
-  const show = S.activeTab === 'servers' && S.servers.length > 10;
+  const show = S.activeTab === 'servers' && S.servers.length > 20;
   $('serversToolbar').style.display = show ? 'flex' : 'none';
 }
 
